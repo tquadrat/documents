5e87e1a5745db09253e15fb5edf5e49d0109a5a6
Thomas Thrien <thomas.thrien@tquadrat.org>
2023-04-03 14:21:32 +0200

    Continued the rework of the existing sections of chapter 5 'Coding Guideline'

--- src/build.sh
+++ src/build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-pdflatex -draftmode JavaCodingConventions > /dev/nul
-pdflatex -draftmode JavaCodingConventions > /dev/nul
+pdflatex -draftmode JavaCodingConventions > /dev/null
+pdflatex -draftmode JavaCodingConventions > /dev/null
 biber JavaCodingConventions
 makeindex -s idxhdr.ist JavaCodingConventions
 pdflatex JavaCodingConventions 
-thumbpdf JavaCodingConventions
+thumbpdf JavaCodingConventions > /dev/null
 pdflatex JavaCodingConventions 
 
